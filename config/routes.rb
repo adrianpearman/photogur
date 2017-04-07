5602439e41589e38b1813ffff6a06ada4ae31114
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-07 16:50:03 -0400

    Refractoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  root 'pictures#index'
-  get 'pictures' => 'pictures#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'pictures#index'
+  resources :pictures
+
+# this is the long form method of the code listed below
+  # root 'pictures#index'
+  # get 'pictures' => 'pictures#index'
+  #
+  # post 'pictures' => 'pictures#create' # this is a new line of code
+  # get 'pictures/new' => 'pictures#new' # this is also a new line of code
+  #
+  # get 'pictures/:id' => 'pictures#show', as: 'picture'
+  #
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # patch 'pictures/:id' => "pictures#update"
+  #
+  # delete'picture/:id' => 'pictures#destroy', as: "delete_picture"
+  # root 'pictures#index'
+
 end
 
-Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
-
-    post 'pictures' => 'pictures#create' # this is a new line of code
-    get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-    get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-    get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-    patch 'pictures/:id' => "pictures#update"
-
-    delete'picture/:id' => 'pictures#destroy', as: "delete_picture"
-    root 'pictures#index'
-
-end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
